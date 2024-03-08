5ae6f8f44780b83ff36cae156c3d99335540afd4
Jake Hattwell <stuck@sixpm.dev>
2024-03-08 13:00:03 +1000

    make wallet name easier to see

--- app/WalletName.tsx
+++ app/WalletName.tsx
@@ -1,12 +1,11 @@
-'use client'; //Use client to access context
+"use client"; //Use client to access context
 import { useContext } from "react";
 import { WalletContext } from "@/app/_session/WalletProvider";
-export default function SComp(){
-    
-    let {kit, session, setSession} = useContext(WalletContext);
-    return (
-        <div>
-        <p>{session ? (session.actor.toString()) : ("Not logged in")}</p>
-        </div>
-    )
+export default function SComp() {
+  let { kit, session, setSession } = useContext(WalletContext);
+  return (
+    <div className="bg-blue-900 border-white text-white border-2 rounded-lg">
+      <p>{session ? session.actor.toString() : "Not logged in"}</p>
+    </div>
+  );
 }
